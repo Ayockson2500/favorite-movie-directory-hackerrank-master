6df9203d353866ebc80a7f663e6dfd049d03d8bb
ayockson2500 <ayockson2500@gmail.com>
2022-11-09 22:42:40 +0100

    changes made

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Movieform from "./Movieform";
 import Movieslist from "./Movieslist";
 import Search from "./Search";
+// import Movieform from "./Movieform";
 
 export { Movieform, Movieslist, Search };
